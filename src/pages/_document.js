e842531e50f7cc1d0f7e7da181206d78fe9f1664
himanshubhadoria2727 <hbhadoria78@gmail.com>
2024-08-21 13:59:38 +0530

    meta tag for testing

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -31,6 +31,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang='en-GB'>
         <Head>
+        <meta name="goodblogger-verification" content="2624749c-4d44-4f4c-a2ad-c44b2cb5faeb"/>
           <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
         </Head>
         <body>
